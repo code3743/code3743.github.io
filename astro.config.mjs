445ec1161d7a24ba56f20332dea03e0c52e7174b
Jota López <102370043+code3743@users.noreply.github.com>
2025-03-22 14:52:28 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://code3743.github.io',
+  site: 'https://jotalopez.dev',
   integrations: [tailwind()]
 });
